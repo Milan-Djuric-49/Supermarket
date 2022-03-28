a82e48ec9e09e1b3f9328076353b4c89aa792a6c
Milan-Djuric <milan.djuric@prvabeogim.edu.rs>
2022-03-28 19:24:55 +0200

    konekcija

---
+++ Supermarket/Konekcija.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Supermarket
+{
+    class Konekcija
+    {
+        static public SqlConnection Povezi()
+        {
+            string CS = ConfigurationManager.ConnectionStrings["home"].ConnectionString;
+            SqlConnection veza = new SqlConnection(CS);
+            return veza;
+        }
+    }
+}
